3c580b820f1ea60e67a2d4bc1134c77fd22dc2ca
LUISAZNAGASC <PerfilProfesional_LuisAznarezGascon@outlook.es>
2025-02-28 13:50:34 +0100

    VisualStudio_DesignPatternsInCSharp : NEW UPDATE

---
+++ Scripts/CreationalDesignPatterns/PrototypeDesignPattern/RegularPrototypeDesignPattern/IPrototypeComponent_RegularPrototype.cs
@@ -0,0 +1,10 @@
+﻿namespace VisualStudio_DesignPatternsInCSharp.Scripts.CreationalDesignPatterns.PrototypeDesignPattern.RegularPrototypeDesignPattern
+{
+    public interface IPrototypeComponent<TemplateComponent1>
+        where TemplateComponent1 : class, IPrototypeComponent<TemplateComponent1>
+    {
+        public TemplateComponent1 GetPrototypeComponentClonation();
+
+        public void SetPrototypeComponentClonation(in TemplateComponent1 prototypeComponentClonation);
+    }
+}
